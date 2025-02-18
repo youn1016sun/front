5bc11295b9bb5aca5c0b49ac5d437a553317f907
youn1016sun <youn1016king@gmail.com>
2025-02-18 15:12:57 +0900

    Fix: History

--- src/api/BaseApi.tsx
+++ src/api/BaseApi.tsx
@@ -4,7 +4,7 @@ const BaseApi = axios.create({
     //baseURL: "http://222.100.174.159:25565/api/",
     //baseURL: "https://virtserver.swaggerhub.com/TNSTKD98/Algo_Reivew/1.0.0/api",
     //baseURL: "http://52.199.8.118:8000/api",
-    baseURL: "http://52.199.8.118:8000/api",
+    baseURL: "https://virtserver.swaggerhub.com/TNSTKD98/Algo_Reivew/1.0.0/api",
     headers: { "Content-Type": "application/json" },
 });
 
