1871adc822b184978eae4932ab4f03952d49fd6d
HyeongSikkk <2674625@naver.com>
2025-02-21 16:30:20 +0900

    사이드바 히스토리 바로 보이는 기능 구현

--- src/layouts/ReviewLayout.tsx
+++ src/layouts/ReviewLayout.tsx
@@ -12,15 +12,16 @@ const ReviewLayout: React.FC<ReviewLayoutProps> = ({ children }) => {
   const location = useLocation();
   const userId = location.state?.userId || localStorage.getItem("user_id");
   const [selectedHistoryId, setSelectedHistoryId] = useState<number | null>(null);
+  const [histories, setHistories] = useState<any>([]);
 
   return (
     <div className="review-layout">
       <Header />
       <div className="review-container">
-        <CustomSidebar userId={userId} onSelectHistory={setSelectedHistoryId} />
+        <CustomSidebar userId={userId} onSelectHistory={setSelectedHistoryId} histories={histories} setHistories={setHistories} />
         <main className="review-content">
           {React.isValidElement(children)
-            ? cloneElement(children as ReactElement<any>, { selectedHistoryId })
+            ? cloneElement(children as ReactElement<any>, { selectedHistoryId, histories, setHistories })
             : children}
         </main>
       </div>
