a084d289db56824a1fb8e3226ac915987e979deb
HyeongSikkk <2674625@naver.com>
2025-02-22 10:23:48 +0900

    리뷰 페이지 디자인 대폭 변경

--- src/api/BaseApi.tsx
+++ src/api/BaseApi.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const BaseApi = axios.create({
-    baseURL: "http://222.100.174.159:25565/api/",
+    baseURL: "http://13.112.208.93:8000/api/",
     // 스웨거 서버 baseURL: "https://virtserver.swaggerhub.com/TNSTKD98/Algo_Reivew/1.0.0/api",
     // 형식님 서버 http://222.100.174.159:25565/api/
-    // 최신 서비스 서버 URL: http://52.195.10.218:8000/api/
+    // 최신 서비스 서버 URL: http://13.112.208.93:8000/api/
     headers: { "Content-Type": "application/json" },
 });
 
