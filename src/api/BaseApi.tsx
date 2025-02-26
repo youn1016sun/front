5963dfb421953257f51c2773bdff67f359c618cf
Jang Min Sung <88541670+masterjms@users.noreply.github.com>
2025-02-26 11:12:14 +0900

    homepage 스크롤바 오류 수정

--- src/api/BaseApi.tsx
+++ src/api/BaseApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BaseApi = axios.create({
-    baseURL: "http://54.199.32.178:8000/api",
+    baseURL: "https://virtserver.swaggerhub.com/TNSTKD98/Algo_Reivew/1.0.0/api",
     // 스웨거 서버 baseURL: "https://virtserver.swaggerhub.com/TNSTKD98/Algo_Reivew/1.0.0/api",
     // 형식님 서버 http://222.100.174.159:25565/api
     // 최신 서비스 서버 URL: http://54.199.32.178:8000/api
